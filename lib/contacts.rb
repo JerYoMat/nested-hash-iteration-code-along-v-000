5a8f125e5afb8f3b7b977d22757a176f1ca3d7d7
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-29 16:50:44 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
 
   contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
     contact_details_hash.each do |category, data|
       if category == :favorite_ice_cream_flavors
         data.collect do |flavor|
